7489f660f155c7f49c4374132f1467348b5b4ab7
matheusvaladao <124572034+matheusvaladao@users.noreply.github.com>
2023-03-16 20:01:30 -0300

    feat:backend finalizado

--- models/User.ts
+++ models/User.ts
@@ -1,9 +1,9 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, {Schema} from 'mongoose';
 
 const UserSchema = new Schema({
-    name : {type: String, require: true},
-    email : {type: String, require: true},
-    password : {type: String, require: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
 });
 
 export const UserModel = mongoose.models.users 
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,64 +1,61 @@
-import { UserModel } from './../../models/User';
-import { DefaultMsgResponse } from '../../types/DefaultMsgResponse';
-import type { NextApiRequest, NextApiResponse   } from "next";
-import { connectToDB } from '@/middlewares/dbconnection';
+import { connectToDB } from './../../middlewares/connectToDB';
+import { DefaultMessageResponse } from './../../types/DefaultMessageResponse';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { UserModel } from '@/models/User';
 import CryptoJs from 'crypto-js';
 
 type Register = {
     name: string,
     email: string,
-    password: string
+    password : string
 }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<DefaultMsgResponse>) => {
-
+const handler = async (req : NextApiRequest, res : NextApiResponse<DefaultMessageResponse>) => {
     try {
-
-        if(req.method !== 'POST') {
+        if(req.method !== 'POST'){
             return res.status(405).json({error: 'Método solicitado não existe!'});
         }
 
         const {MY_SUPER_SECRET_KEY} = process.env;
-        if(!MY_SUPER_SECRET_KEY) {
-            res.status(500).json({error: 'ENV MY_SUPER_SECRET_KEY não preenchida'});
+        if(!MY_SUPER_SECRET_KEY){
+            console.log('ENV MY_SUPER_SECRET_KEY não preenchida');
             return res.status(500).json({error : 'ENV MY_SUPER_SECRET_KEY não preenchida'});
         }
 
         const {name, email, password} = req.body as Register;
 
-        if(!name || name.trim().length < 2){
-            return res.status(400).json({error: 'Nome inválido'});
+        if(!name || name.trim().length < 2 ){
+            return res.status(400).json({error: 'Nome inválido!'});
         }
 
         if(!email || email.trim().length < 6 || !email.includes('@') || !email.includes('.')){
-            return res.status(400).json({error: 'Email inválido'});
+            return res.status(400).json({error: 'Email inválido!'});
         }
 
-        const existUser = await UserModel.findOne({email});
-        if(existUser) {
-            return res.status(400).json({error: 'Email já cadastrado'});
+        const existsUser = await UserModel.findOne({email});
+        if(existsUser){
+            return res.status(400).json({error: 'Email já cadastrado!'});
         }
 
-        if(!password || password.trim().length < 4){
-            return res.status(400).json({error: 'Senha inválida'});
+        if(!password || password.trim().length < 4 ){
+            return res.status(400).json({error: 'Senha inválida!'});
         }
 
         const passwordCyphered = CryptoJs.AES.encrypt(password, MY_SUPER_SECRET_KEY).toString();
 
         const user = {
-            name, 
+            name,
             email,
             password: passwordCyphered
-        }
+        };
+
         await UserModel.create(user);
 
-        return res.status(200).json({msg: 'Usuário cadastrado'});
-
+        return res.status(200).json({msg: 'Usuário cadastrado!'});
     } catch (ex) {
-        console.log('Ocorreu erro ao cadastrar usuário: ', ex);
-        res.status(500).json({error: 'Ocorreu erro ao efetuar o login, tente novamente!'});
+        console.log('Ocorreu erro ao cadastrar usuário:', ex);
+        res.status(500).json({error: 'Ocorreu erro ao cadastrar usuário, tente novamente!'});
     }
-
 }
 
 export default connectToDB(handler);
