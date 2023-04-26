6e89001ba427b4b2c4404fe3997ffc541e090cb2
matheusvaladao <124572034+matheusvaladao@users.noreply.github.com>
2023-04-25 21:14:16 -0300

    feat:Filtro Mobile

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 
 type FilterProps = {
     previsionDateStart: string,
@@ -18,13 +19,14 @@ export const Filter: NextPage<FilterProps> = ({
     setStatus
 }) => {
 
+    const [showFilter, setShowFilter] = useState(false);
 
     return (
         <div className='container-filter'>
 
             <div className='title'>
                 <span>Tarefas</span>
-                <img src='/filter.svg' alt='Filtrar tarefas' />
+                <img src='/filter.svg' alt='Filtrar tarefas' onClick={_ => setShowFilter(!showFilter)} />
                 <div className='form'>
                     <div>
                         <label>Data prevista de conclusão:</label>
@@ -45,6 +47,30 @@ export const Filter: NextPage<FilterProps> = ({
                     </div>
                 </div>
             </div>
+
+            {showFilter && <div className='mobile-filter'>
+
+                
+                    <div>
+                        <label>Data prevista de conclusão:</label>
+                        <input type="date" value={previsionDateStart} onChange={e => setPrevisionDateStart(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>até</label>
+                        <input type="date" value={previsionDateEnd} onChange={e => setPrevisionDateEnd(e.target.value)} />
+                    </div>
+                    <div className='separator' />
+                    <div>
+                        <label>Status</label>
+                        <select value={status} onChange={e => setStatus(parseInt(e.target.value))}>
+                            <option value={0}>Todas</option>
+                            <option value={1}>Ativas</option>
+                            <option value={2}>Concluídas</option>
+                        </select>
+                    </div>
+                
+
+            </div>}
 
         </div>
     );
